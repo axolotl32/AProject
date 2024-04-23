bd1a460cdd990bb0075837bd30af5948fff92c35
axolotl32 <167874994+axolotl32@users.noreply.github.com>
2024-04-23 14:26:38 +0100

    Serve Static CSS Files on Vercel
    Issue with /public route not being recognized, therefore CSS styling was not applied.

--- src/index.js
+++ src/index.js
@@ -58,6 +58,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(express.static(__dirname + "/public/"));
 app.use(bodyParser.json());
 app.use(session({
   secret: process.env.SESSION_SECRET,
